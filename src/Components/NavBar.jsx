e32b3572d226673203a1c5988fb82e588a55b0f6
Boa Matule <boa.matule@gmail.com>
2019-05-23 13:28:01 +0200

    Adds second navbar category.

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,18 +1,35 @@
 import React from 'react';
 
+
 const NavBar = () => {
   return (
+    <>
     <div>
-      
-      <h1>Glocal News</h1>
+        <h1>Glocal News</h1>
         <h2>Welcome Member</h2>
         <h2>WRITE AN ARTICLE</h2>
         <h2>REVIEW ARTICLES</h2>
         <h2>Sign Up</h2>
         <h2>Login</h2>
         <h2>Log out</h2>
-
     </div>
+
+    <div>
+      <h3>News</h3>
+      <h3>Arts</h3>      
+      <h3>Books</h3>      
+      <h3>Business</h3>      
+      <h3>Food</h3>  
+      <h3>Opinion</h3>
+      <h3>Politics</h3> 
+      <h3>Real Estate</h3>
+      <h3>Science</h3>
+      <h3>Sports</h3>       
+      <h3>Style</h3>      
+      <h3>Tech</h3>        
+      <h3>Travel</h3>
+    </div> 
+  </>
   )
 }
 export default NavBar
