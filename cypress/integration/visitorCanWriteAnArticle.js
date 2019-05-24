3f499088124feb25bdab78f2405248bc20bec050
zanenkn <zane.neikena@gmail.com>
2019-05-24 13:43:04 +0200

    Adds a test for successfully creating an article

---
+++ cypress/integration/visitorCanWriteAnArticle.js
@@ -0,0 +1,19 @@
+describe('Visitor can write an article', () => {
+  it ('successfully', () => {
+    cy.visit('http://localhost:3001')
+    cy.get('#write_article').click()
+    cy.get('#written_by').type('Boa Matule')
+    cy.get('#title').type('Rainy day')
+    cy.get('#ingress').type('Today it rained.')
+    cy.get('#body').type('Rain is good for flowers.')
+    cy.get('#image').type('https://github.com')
+    cy.get('#create').click()
+    cy.route({
+      method: 'POST',
+      url: 'http://localhost:3000/api/v1/articles',
+      response: 'Successfully created',
+      status: 200
+    })
+    cy.contains('Your article is under review process.')
+  })
+})
