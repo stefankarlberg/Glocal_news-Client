6e1956d7c033065266c86cf5d7f9b4c198a5fe3a
felix_bonnier <felix_bonnier@yahoo.com>
2019-05-24 14:57:45 +0200

    adds first Form input field for 'title' in writeArticle component.

--- src/Components/WriteArticle.jsx
+++ src/Components/WriteArticle.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Form, Container } from 'semantic-ui-react'
 
 const WriteArticle = () => {
   return (
-    <p>yo!</p>
+    <>
+      <Container>
+        <Form type="medium" id="write-article">
+          <Form.Input
+            fluid
+            id="title"
+            placeholder="Title"
+            />
+        </Form>
+      </Container>
+    </>
   )
 }
 
