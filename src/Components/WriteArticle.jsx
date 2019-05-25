e0e43d42b9cc335a088adfc57ec25a040bdf1183
zanenkn <zane.neikena@gmail.com>
2019-05-25 11:02:51 +0200

    Adds onChangeHandler and onSubmit functions to the WriteAricle component, deletes the modules since the POST request is now part of onSubmit. Adds axios.defaults.baseURL to the index.js

--- src/Components/WriteArticle.jsx
+++ src/Components/WriteArticle.jsx
@@ -1,41 +1,80 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Form, Container, Button } from 'semantic-ui-react'
+import axios from 'axios'
 
-const WriteArticle = () => {
-  return (
-    <>
-      <Container>
-        <Form type="medium" id="write-article">
-          <Form.Input
-            fluid
-            id="title"
-            placeholder="Title"
-          />
-          <Form.TextArea
-            fluid
-            id="ingress"
-            placeholder="Ingress"
-          />
-          <Form.TextArea
-            fluid
-            id="body"
-            placeholder="Body"
+class WriteArticle extends Component {
+  state = {
+    title: '',
+    ingress: '',
+    body: '',
+    image: '',
+    written_by: ''
+  }
+
+  onChangeHandler = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value
+    })
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    const path = '/api/v1/articles'
+    const payload = {...this.state}
+    axios.post(path, payload)
+      .then(response => {
+        console.log(response)
+      })
+  }
+  
+
+
+  render () {
+    return (
+      <>
+        <Container>
+          <Form type="medium" id="write-article" onSubmit={this.onSubmit}>
+            <Form.Input
+              fluid
+              id="title"
+              value={this.state.title}
+              onChange={this.onChangeHandler}
+              placeholder="Title"
             />
-          <Form.Input
-            fluid
-            id="written_by"
-            placeholder="Written By"
-          />
-          <Form.Input
-            fluid
-            id="image"
-            placeholder="https://image.com"
-          />
-          <Button id="create">Create Article</Button>
-        </Form>
-      </Container>
-    </>
-  )
+            <Form.TextArea
+              fluid
+              id="ingress"
+              value={this.state.ingress}
+              onChange={this.onChangeHandler}
+              placeholder="Ingress"
+            />
+            <Form.TextArea
+              fluid
+              id="body"
+              value={this.state.body}
+              onChange={this.onChangeHandler}
+              placeholder="Body"
+              />
+            <Form.Input
+              fluid
+              id="written_by"
+              value={this.state.written_by}
+              onChange={this.onChangeHandler}
+              placeholder="Written By"
+            />
+            <Form.Input
+              fluid
+              id="image"
+              value={this.state.image}
+              onChange={this.onChangeHandler}
+              placeholder="https://image.com"
+            />
+            <Button id="create">Create Article</Button>
+          </Form>
+        </Container>
+      </>
+    )
+  }
 }
 
 export default WriteArticle
--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import 'semantic-ui-css/semantic.min.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from "react-router-dom"
+import axios from 'axios'
 
-
+axios.defaults.baseURL = 'http://localhost:3000'
 ReactDOM.render((
   <BrowserRouter>
       <App />
