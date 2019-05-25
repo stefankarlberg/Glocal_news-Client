338f3a70e24ae9bb81ec00a7fdff07bc7fba35de
zanenkn <zane.neikena@gmail.com>
2019-05-25 16:52:51 +0200

    Adds a sad path test for a scenario where visitor does not fill in all the fields while creating an article.

--- cypress/integration/visitorCanWriteAnArticle.js
+++ cypress/integration/visitorCanWriteAnArticle.js
@@ -1,7 +1,10 @@
-describe('Visitor can write an article', () => {
-  it ('successfully', () => {
+describe('Visitor can', () => {
+  beforeEach(function() {
     cy.visit('http://localhost:3001')
     cy.get('#write_article').click()
+  })
+  
+  it ('write an article successfully', () => {
     cy.get('#title').type('Rainy day')
     cy.get('#ingress').type('Today it rained.')
     cy.get('#body').type('Rain is good for flowers.')
@@ -16,5 +19,21 @@ describe('Visitor can write an article', () => {
       status: 200
     })
     cy.contains('Thank you for sharing your story! Your article is awaiting reviews.')
+  })
+
+  it ('not create an article if all fields are not filled in', () => {
+    cy.get('#title').type('Sunny day')
+    cy.get('#ingress').type('Today the sun is shining.')
+    cy.get('#written_by').type('Boa Matule')
+    cy.get('#image').type('https://github.com')
+    cy.get('#create').click()
+    cy.server();
+    cy.route({
+      method: 'POST',
+      url: 'http://localhost:3000/api/v1/articles',
+      response: "Body can't be blank",
+      status: 422
+    })
+    cy.contains("Your article could not be created because of following error(s): Body can't be blank")
   })
 })
