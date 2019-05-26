008887b4533c132de748d1a9e09593dd213de8df
raptorf1 <raptor_f1@hotmail.com>
2019-05-26 22:16:36 +0200

    Edits tests to accommodate styling changes. Only 'visitorCanReadArticleTest' not passing cause of mock data problem. Tests with live API are OK. Tests with localhost:3000 also OK.

--- cypress/integration/visitorCanNavigateTheSite.spec.js
+++ cypress/integration/visitorCanNavigateTheSite.spec.js
@@ -1,42 +1,41 @@
 describe('Visitor can navigate the website', () => {
-  beforeEach(function() {
+  beforeEach(function () {
     cy.visit('http://localhost:3001');
   })
 
-  it ("by seeing a navbar with site title, article -and review actions", () => {
-    let mainLabels = ["Glocal News", "Write An Article", "Review Articles"]
-  
-    mainLabels.forEach(function(label){
-    cy.contains(label)
+  it("by seeing a navbar with site title, article -and review actions", () => {
+    let mainLabels = ["GLOCAL NEWS", "Write An Article", "Review Articles"]
+
+    mainLabels.forEach(function (label) {
+      cy.contains(label)
     })
   })
 
-  it ("by seeing login and logout information", () => {  
+  it("by seeing login and logout information", () => {
     let loggedOutLabels = ['Sign Up', 'Log In']
-    let loggedInLabels = ['Welcome Member', 'Log Out'] 
-    
-    loggedInLabels.forEach(function(label){
+    let loggedInLabels = ['Welcome Member', 'Log Out']
+
+    loggedInLabels.forEach(function (label) {
       cy.contains(label)
     })
 
-    loggedOutLabels.forEach(function(label){
+    loggedOutLabels.forEach(function (label) {
       cy.contains(label)
     })
   })
 
-  it ('by seeing a second selection with categories in the navbar', () => {
+  it('by seeing a second selection with categories in the navbar', () => {
     let categories = ["News", "Arts", "Books", "Business", "Food", "Opinion",
-                      "Politics", "Real Estate", "Science", "Sports", "Style",
-                      "Tech", "Travel"]
+      "Politics", "Real Estate", "Science", "Sports", "Style",
+      "Tech", "Travel"]
 
-    categories.forEach(function(category){
+    categories.forEach(function (category) {
       cy.contains(category)
     })
   })
 
-  it ("by seeing correct selection options in dropdown", () => {
+  it("by seeing correct selection options in dropdown", () => {
     cy.get('div[id="country"]').click().get('div[role="option"]').contains('Sweden');
     cy.get('div[id="city"]').click().get('div[role="option"]').contains('Stockholm')
   })
-  
 })
--- cypress/integration/visitorCanReadAnArticle.spec.js
+++ cypress/integration/visitorCanReadAnArticle.spec.js
@@ -9,14 +9,14 @@ describe('Visitor can read an article', () => {
       status: 200
     })
     cy.visit('http://localhost:3001')
-    cy.get("#1").click()
     cy.server();
     cy.route({
       method: 'GET',
-      url: 'http://localhost:3000/api/v1/articles',
+      url: 'http://localhost:3000/api/v1/articles/1',
       response: 'fixture:full_article.json',
       status: 200
     })
+    cy.get("#1").click()
     cy.contains('A Day in Stockholm')
     cy.contains('Hot to spend 24 hours in Stockholm')
     cy.contains('Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?')
