0bc2d8c788a9b377ce5bd03ce59e67f1d34d58f2
raptorf1 <raptor_f1@hotmail.com>
2019-05-26 15:04:06 +0200

    Installs momentjs package. Configures 'FullArticle' component to accept new date. All tests passing.

--- src/Components/FullArticle.jsx
+++ src/Components/FullArticle.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import { Divider, Segment, Header, Image, Container } from 'semantic-ui-react'
+import moment from 'moment'
 
 class FullArticle extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      id: '',
       title: '',
       ingress: '',
       body: '',
@@ -18,8 +20,9 @@ class FullArticle extends Component {
   componentDidMount() {
     let mainPath = '/api/v1/articles/'
     let articlePath = (this.props.location.state.id)
-    axios.get(mainPath+articlePath).then(response => {
+    axios.get(mainPath + articlePath).then(response => {
       this.setState({
+        id: response.data.id,
         title: response.data.title,
         ingress: response.data.ingress,
         body: response.data.body,
@@ -32,28 +35,34 @@ class FullArticle extends Component {
 
 
   render() {
+
+    let dateString = this.state.created_at;
+    let dateObj = new Date(dateString);
+    let momentObj = moment(dateObj);
+    let momentString = momentObj.format('YYYY-MM-DD');
+
     return (
       <>
         <Container>
 
           <Segment padded>
             <Header as="h1">{this.state.title}</Header>
-            
-            <Divider/>
 
-            <Image src={this.state.image} size="large" floated="left"></Image>
+            <Divider />
 
-            <p style={{fontSize: "1.2em"}}>{this.state.ingress}</p>
+            <Image id={`photo_${this.state.id}`} src={this.state.image} size="large" floated="left"></Image>
+
+            <p style={{ fontSize: "1.2em" }}>{this.state.ingress}</p>
             <p>{this.state.body}</p>
 
-            <Divider/>
+            <Divider />
 
             <strong>Written by: {this.state.written_by}</strong>
             <br></br>
-            <i>{this.state.created_at}</i>
+            <i>{momentString}</i>
 
           </Segment>
-              
+
         </Container>
       </>
     )
