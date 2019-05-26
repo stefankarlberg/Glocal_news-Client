8db41588e503a58281acab6ec3b438dac70c6cfb
raptorf1 <raptor_f1@hotmail.com>
2019-05-26 15:35:43 +0200

    Adjusts all tests to work with mock data. 2/3 passing. Indents code where needed.

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'https://glocal-news.herokuapp.com/'
+axios.defaults.baseURL = 'http://localhost:3000/'
 ReactDOM.render((
   <BrowserRouter>
     <App />
   </BrowserRouter>
-  ), document.getElementById('root'));
+), document.getElementById('root'));
 
 serviceWorker.unregister();
