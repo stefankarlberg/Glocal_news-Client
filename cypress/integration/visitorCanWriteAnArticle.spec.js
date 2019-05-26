b08c5d949d6165ff9863ed69ad60b6fb3be281c7
raptorf1 <raptor_f1@hotmail.com>
2019-05-26 22:41:15 +0200

    Adjusts acceptance tests and API mock data. All tests green.

--- cypress/integration/visitorCanWriteAnArticle.spec.js
+++ cypress/integration/visitorCanWriteAnArticle.spec.js
@@ -1,5 +1,5 @@
 describe('Visitor can', () => {
-  beforeEach(function() {
+  beforeEach(function () {
     cy.server();
     cy.route({
       method: 'GET',
@@ -10,30 +10,28 @@ describe('Visitor can', () => {
     cy.visit('http://localhost:3001')
     cy.get('#write_article').click()
   })
-  
-  it ('write an article successfully', () => {
+
+  it('write an article successfully', () => {
     cy.get('#title').type('Rainy day')
     cy.get('#ingress').type('Today it rained.')
     cy.get('#body').type('Rain is good for flowers.')
     cy.get('#written_by').type('Boa Matule')
     cy.get('#image').type('https://github.com')
-    cy.get('#create').click()
     cy.server();
     cy.route({
       method: 'POST',
       url: 'http://localhost:3000/api/v1/articles',
-      response: 'fixture:create_article_success.json',
-//      status: 200
+      response: 'fixture:create_article_success.json'
     })
+    cy.get('#create').click()
     cy.contains('Thank you for sharing your story! Your article is awaiting reviews.')
   })
 
-  it ('not create an article if all fields are not filled in', () => {
+  it('not create an article if all fields are not filled in', () => {
     cy.get('#title').type('Sunny day')
     cy.get('#ingress').type('Today the sun is shining.')
     cy.get('#written_by').type('Boa Matule')
     cy.get('#image').type('https://github.com')
-    cy.get('#create').click()
     cy.server();
     cy.route({
       method: 'POST',
@@ -41,6 +39,7 @@ describe('Visitor can', () => {
       response: 'fixture:create_article_no_success.json',
       status: 422
     })
+    cy.get('#create').click()
     cy.contains("Your article could not be created because of following error(s):")
     cy.contains("Body can't be blank")
   })
