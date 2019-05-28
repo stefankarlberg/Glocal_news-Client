87882bea36d3f6fdb9cc8a3b719f67157881c15d
stefan_karlberg <stefan.karlberg@smackit.se>
2019-05-28 16:05:59 +0200

    Creates component for listing of unpublished articles. Adds link in HeaderMain component for review articles page. Adds routes in app.js. Test displaying correct header and redirects correctly.

---
+++ src/Components/ListOfUnpublishedArticles.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Header } from 'semantic-ui-react'
+
+const ListOfUnpublishedArticles = () => {
+  return (
+    <>
+      <Header>
+        Unpublished Articles
+      </Header>
+    </>
+  )
+}
+
+export default ListOfUnpublishedArticles 
