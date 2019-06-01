d79676c98ed795a5e90f45294cacfd477316729b
Boa Matule <boa.matule@gmail.com>
2019-06-01 14:30:39 +0200

    creates integration test for succesfully rendering the right articles under each category tab. Updates list_of_articles fixtures file to include categories.

---
+++ cypress/integration/visitorCanFilterOnCategories.spec.js
@@ -0,0 +1,39 @@
+describe('Visitor can view articles filtered by category', () => {
+
+  it('successfully', () => {
+    cy.server();
+    cy.route({
+      method: 'GET',
+      url: 'http://localhost:3002/api/v1/articles',
+      response: 'fixture:list_of_articles.json',
+      status: 200
+    })
+    cy.visit('http://localhost:3001')
+    cy.get('#politics').click()
+
+    let politics = [
+      ["#36", "#title_36", "#ingress_36", "#photo_36"],
+      ["#37", "#title_37", "#ingress_37", "#photo_37"],
+    ]
+
+    politics.forEach(article => {
+      cy.get(article[0]).within(() => {
+        cy.get(article[1]), (article[2])
+        cy.get(article[3]).should('have.attr', 'src')
+      })
+    })
+
+    cy.get('#arts').click()
+
+    let arts = [
+      ["#38", "#title_38", "#ingress_38", "#photo_38"],
+    ]
+
+    arts.forEach(article => {
+      cy.get(article[0]).within(() => {
+        cy.get(article[1]), (article[2])
+        cy.get(article[3]).should('have.attr', 'src')
+      })
+    })
+  })  
+})
