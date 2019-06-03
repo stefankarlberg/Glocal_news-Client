f6c9c3b1b0e038cd1e41d0c2079be1efcc99ff9c
zanenkn <zane.neikena@gmail.com>
2019-06-03 14:24:01 +0200

    WIP separate component for fetching categories from API

--- src/App.js
+++ src/App.js
@@ -1,32 +1,50 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ListOfArticles from './Components/ListOfArticles';
 import HeaderMain from './Components/HeaderMain.jsx'
 import WriteArticle from './Components/WriteArticle'
 import ListOfUnpublishedArticles from './Components/ListOfUnpublishedArticles'
 import { Switch, Route } from 'react-router-dom'
 import FullArticle from './Components/FullArticle'
-import ArticlesByCategory from './Components/ArticlesByCategory';
+import ArticlesByCategory from './Components/ArticlesByCategory'
+import getCategories from './Components/CategoriesData'
 
-const categories = ['News', 'Arts', 'Books', 'Business', 'Food', 'Opinion', 'Politics', 'Real Estate', 'Science', 'Sports', 'Style', 'Tech', 'Travel']
+// const categories = ['News', 'Arts', 'Books', 'Business', 'Food', 'Opinion', 'Politics', 'Real Estate', 'Science', 'Sports', 'Style', 'Tech', 'Travel']
 
-const categoryPaths = []
-categories.forEach(category => {
-  categoryPaths.push(`/${category}`)
-})
+// const categoryPaths = []
+// categories.forEach(category => {
+//   categoryPaths.push(`/${category}`)
+// })
 
-const App = () => {
-  return (
-    <>
-      <HeaderMain />
-        <Switch>
-          <Route exact path='/' component={ListOfArticles}></Route>
-          <Route exact path={categoryPaths} component={ArticlesByCategory}></Route>
-          <Route exact path='/write-article' component={WriteArticle}></Route>
-          <Route exact path='/review-articles' component={ListOfUnpublishedArticles}></Route>
-          <Route exact path='/full-article' component={FullArticle}></Route>
-        </Switch>
-    </>
-  );
+
+
+class App extends Component {
+
+  // componentDidMount() {
+  //   this.setState({
+  //     categories: getCategories()
+  //   })  
+  // }
+
+  render() {
+    let categories = getCategories();
+    debugger
+    let categoryNames = categories.map(category => {
+      return category.name && `/${category.name}`
+    })
+    debugger
+    return (
+      <>
+        <HeaderMain />
+          <Switch>
+            <Route exact path='/' component={ListOfArticles}></Route>
+            {/* <Route exact path={categoryPaths} component={ArticlesByCategory}></Route> */}
+            <Route exact path='/write-article' component={WriteArticle}></Route>
+            <Route exact path='/review-articles' component={ListOfUnpublishedArticles}></Route>
+            <Route exact path='/full-article' component={FullArticle}></Route>
+          </Switch>
+      </>
+    );
+  }
 }
 
 export default App;
