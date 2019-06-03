bf7e353df378d3867faa22578d0fab92e876deea
raptorf1 <raptor_f1@hotmail.com>
2019-06-03 13:20:55 +0200

    Adds successfull sign up message

--- src/Components/SignUpForm.jsx
+++ src/Components/SignUpForm.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Form, Button } from 'semantic-ui-react'
+import { Form, Button, Container, Message } from 'semantic-ui-react'
 import { connect } from 'react-redux'
 import { registerUser } from '../reduxTokenAuthConfig'
 
@@ -8,6 +8,7 @@ class SignUpForm extends Component {
     email: '',
     password: '',
     password_confirmation: '',
+    message: false
   }
 
   onChangeHandler = (e) => {
@@ -26,41 +27,58 @@ class SignUpForm extends Component {
     } = this.state
     try {
       await registerUser({ email, password, password_confirmation })
-      history.push('/')
+      this.setState({ message: true })
+      setTimeout(function () { history.push('/') }, 3000)
     } catch (error) { console.log(error) }
   }
 
 
   render() {
 
+    let message
+
+    if (this.state.message === true) {
+      message = (
+        <>
+          <br />
+          <Message color="green">
+            <p>You have succesfully signed up. Wait to be redirected to the main page.</p>
+          </Message>
+        </>
+      )
+    }
+
     return (
-      <Form id="signup-form" onSubmit={this.onSubmit}>
-        <Form.Input
-          id="email"
-          value={this.state.email}
-          onChange={this.onChangeHandler}
-          placeholder="Email"
-        />
+      <Container>
+        {message}
+        <Form id="signup-form" onSubmit={this.onSubmit}>
+          <Form.Input
+            id="email"
+            value={this.state.email}
+            onChange={this.onChangeHandler}
+            placeholder="Email"
+          />
 
-        <Form.Input
-          id="password"
-          type="password"
-          value={this.state.password}
-          onChange={this.onChangeHandler}
-          placeholder="Password"
-        />
+          <Form.Input
+            id="password"
+            type="password"
+            value={this.state.password}
+            onChange={this.onChangeHandler}
+            placeholder="Password"
+          />
 
-        <Form.Input
-          id="password_confirmation"
-          type="password"
-          value={this.state.password_confirmation}
-          onChange={this.onChangeHandler}
-          placeholder="Password Confirmation"
-        />
+          <Form.Input
+            id="password_confirmation"
+            type="password"
+            value={this.state.password_confirmation}
+            onChange={this.onChangeHandler}
+            placeholder="Password Confirmation"
+          />
 
-        <Button id="sign_up">Sign Up</Button>
+          <Button id="sign_up">Sign Up</Button>
 
-      </Form>
+        </Form>
+      </Container>
     )
   }
 }
