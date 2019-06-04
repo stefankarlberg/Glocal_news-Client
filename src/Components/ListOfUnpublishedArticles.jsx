d1ff47d428ba8df24f5a1e416818d72d6c59a37f
Carrosen <carla1.rosen@gmail.com>
2019-06-04 21:53:42 +0200

    Adds column for ads, and minor styling adjustments

--- src/Components/ListOfUnpublishedArticles.jsx
+++ src/Components/ListOfUnpublishedArticles.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { Header, Container, Grid, Image, Card} from 'semantic-ui-react';
+import { Header, Container, Grid, Image, Card, Segment } from 'semantic-ui-react';
 import { Link } from 'react-router-dom'
 
 class ListOfUnpublishedArticles extends Component {
@@ -21,18 +21,18 @@ class ListOfUnpublishedArticles extends Component {
         {this.state.articles.map(article => {
           if (article.published === false) {
             return (
-                  <Card style={{ color: 'black' }} fluid key={article.id} as={Link} to={{ pathname: `/full-article/${article.id}`, state: { success_message: false, review_form: true } }} >
-                    <Grid id={article.id} >
-                      <Grid.Column width={5} >
-                      <Image alt="article logo" id={`photo_${article.id}`} src={article.image} />
-                      </Grid.Column>
-                      <Grid.Column style={{ padding: '30px 30px 30px 10px' }} width={11}>
-                      <Header as='h2' id={`title_${article.id}`}>{article.title}</Header>
-                      <p id={`ingress_${article.id}`}>{article.ingress}</p>
-                      <p id={`country_city_${article.id}`}>{`Country: ${article.country}, City: ${article.city}`} </p>
-                      </Grid.Column>
-                    </Grid>
-                  </Card>
+              <Card color='purple' style={{ color: 'black' }} fluid key={article.id} as={Link} to={{ pathname: `/full-article/${article.id}`, state: { success_message: false, review_form: true } }} >
+                <Grid id={article.id} >
+                  <Grid.Column width={5} >
+                    <Image alt="article logo" id={`photo_${article.id}`} src={article.image} />
+                  </Grid.Column>
+                  <Grid.Column style={{ padding: '30px 30px 30px 10px' }} width={11}>
+                    <Header as='h2' id={`title_${article.id}`}>{article.title}</Header>
+                    <p id={`ingress_${article.id}`}>{article.ingress}</p>
+                    <p id={`country_city_${article.id}`}>{`Country: ${article.country}, City: ${article.city}`} </p>
+                  </Grid.Column>
+                </Grid>
+              </Card>
             )
           }
         })}
@@ -54,12 +54,14 @@ class ListOfUnpublishedArticles extends Component {
           <Header>
             Unpublished Articles (if you see nothing in this page, there are no articles up for review)
           </Header>
-
           <Grid centered columns={2}>
             <Grid.Column width={12}>
               {articleList}
             </Grid.Column>
             <Grid.Column width={4}>
+              <Segment vertical textAlign='center' style={{ background: 'grey', height: '100%' }}>
+                <p>Ads placeholder</p>
+              </Segment>
             </Grid.Column>
           </Grid>
         </Container>
