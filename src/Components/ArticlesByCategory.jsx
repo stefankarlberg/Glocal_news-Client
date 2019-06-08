631d4f0466806b14cce96d448bfdc53b0b98b50d
raptorf1 <raptor_f1@hotmail.com>
2019-06-08 13:03:27 +0200

    Removes comments from ArticlesByCategory component

--- src/Components/ArticlesByCategory.jsx
+++ src/Components/ArticlesByCategory.jsx
@@ -85,33 +85,6 @@ class ArticlesByCategory extends Component {
         </Message>
       )
 
-    // let articleList = filteredArticlesByCountry.length ? (
-    //   <div id="filtered_articles">
-    //     {filteredArticlesByCountry.map(article => {
-    //       if (article.published === true) {
-    //         return (
-    //           <Card fluid key={article.id} as={Link} to={{ pathname: '/full-article', state: { id: `${article.id}` } }} >
-    //             <div id={article.id} style={{ color: 'black' }}>
-    //               <Image fluid alt="article logo" id={`photo_${article.id}`} src={article.image} />
-    //               <Card.Content style={{ padding: '2em' }}>
-    //                 <Card.Header as='h1' id={`title_${article.id}`}>{article.title}</Card.Header>
-    //                 <p id={`ingress_${article.id}`}>{article.ingress}</p>
-    //                 <h5 style={{ color: 'grey' }} id={`country_city_${article.id}`}><Icon name='map marker alternate' />{`${article.city}, ${article.country}`}</h5>
-    //               </Card.Content>
-    //             </div>
-    //           </Card>
-    //         )
-    //       }
-    //     }
-    //     )}
-    //   </div>
-    // ) : (
-    //     <Message>
-    //       <Message.Header>There are no {category} articles at the moment</Message.Header>
-
-
-
-
     let articleList = filteredArticlesByCountry.length ? (
 
       <div>
